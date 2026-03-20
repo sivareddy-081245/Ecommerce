c96bb5c028b70ad646d63e3d7d0fe208e2614894
sivareddy <sivareddy081245@gmail.com>
2026-03-20 17:47:20 +0530

    added product service class

--- src/main/java/com/sivareddy/model/Products.java
+++ src/main/java/com/sivareddy/model/Products.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 public class Products {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
